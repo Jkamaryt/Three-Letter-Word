6fdc9c23c138661f37ba6a3997668e1a802dc416
Jack Kamaryt <jkamaryt46@gmail.com>
2022-10-26 09:31:16 -0500

    Initial Commit

---
+++ Three Letter word/Three_Letter_wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_wordApp.swift
+//  Three Letter word
+//
+//  Created by Jack Kamaryt on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
