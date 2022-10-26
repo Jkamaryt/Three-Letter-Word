f44208f0b05198903fe9916135ac08e9011e10c8
Jack Kamaryt <jkamaryt46@gmail.com>
2022-10-26 09:46:49 -0500

    Created custom letter box

--- Three Letter word/ContentView.swift
+++ Three Letter word/ContentView.swift
@@ -1,1 +1,33 @@
+import SwiftUI
 
+struct ContentBiew: View {
+    @State private var letter = ""
+    @State private var counter = 0
+    let alphabet = "ABCDEFGHIJKLMNOPORSTUVWXYZ"
+    
+    var body: some View {
+        Vstack{
+            Text("Three Letter Word")
+                .font(.title).bold()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+struct customLetterBox: View{
+    let color: Color
+    let text: String
+    var body: some View{
+        Zstack {
+            color
+            Text(text)
+                .font(system(size:90))
+                .fontWeight(.heavy)
+        }
+        .frame(width: 120, height: 120, alignment: .center)
+    }
+}
